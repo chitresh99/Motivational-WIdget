f52e5b2f895679512d0aa9919ad5e6bf0569df4b
chitresh99 <chitreshpoojary990@gmail.com>
2024-04-27 21:27:24 +0530

    implemented the change avatar feature

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 from PyQt5.QtCore import Qt, QTimer
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QMenu, QAction, QVBoxLayout
+import os
 import random
 
 
@@ -12,46 +13,55 @@ class MotivationalPuppet(QWidget):
         self.setWindowFlags(Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint)
         self.setAttribute(Qt.WA_TranslucentBackground)
 
+        # List of image paths
+        self.image_dir = "images"
+        self.image_names = os.listdir(self.image_dir)
+        self.image_paths = [os.path.join(self.image_dir, name) for name in self.image_names]
+        self.current_image_index = 0
+
         # Add puppet image label
         self.label = QLabel(self)
-        pixmap = QPixmap('darkrai.png')  # image for the widget
-        if pixmap.isNull():
-            print("Error loading image")
-
-        # Resizing the image
-        pixmap = pixmap.scaledToWidth(200)
-        self.label.setPixmap(pixmap)
-        self.label.setContentsMargins(0, 0, 0, 0)  # Remove padding around the image
+        self.load_image()  # Load initial image
 
         self.notification_label = QLabel("", self)
         self.notification_label.setAlignment(Qt.AlignCenter)
         self.notification_label.setStyleSheet(
-            "background-color: rgba(255, 255, 255, 50); color: blue; font-size: 16px; font-weight: bold; font-family: Arial;")
+            "background-color: rgba(255, 255, 255, 50); color: blue; font-size: 16px; font-weight: bold; font-family: "
+            "Arial;")
 
-        # Add a button to start the quote change
+        # Button to change quotes
         self.start_button = QPushButton("Start", self)
         self.start_button.clicked.connect(self.start_quote_timer)
 
-        # Setting up the layout
-        layout = QVBoxLayout()
+        # Button to change the avatar
+        self.change_avatar_button = QPushButton("Change Avatar", self)
+        self.change_avatar_button.clicked.connect(self.change_avatar)
+
+        # layout
+        layout = QVBoxLayout(self)  # Use the QWidget as the parent
         layout.addWidget(self.notification_label)  # Notification label added first
         layout.addWidget(self.label)
         layout.addWidget(self.start_button)
-        self.setLayout(layout)
+        layout.addWidget(self.change_avatar_button)
 
-        self.resize(250, 250)  # size to fit the resized image and notification label
-
-        # center position
-        x_pos = (self.width() - pixmap.width()) // 2
-        y_pos = (self.height() - pixmap.height()) // 2
-
-        self.label.setGeometry(x_pos, y_pos, pixmap.width(), pixmap.height())
-        self.notification_label.setGeometry(0, 0, self.width(), 20)  # Positioning notification label at the top
-        self.start_button.setGeometry(0, self.height() - 30, self.width(), 30)
-
-        # Staring the QTimer for quote updates
+        # Staring timer for quote updates
         self.quote_timer = QTimer(self)
         self.quote_timer.timeout.connect(self.show_quote)
+
+    def load_image(self):
+        # Load image based on current index
+        pixmap = QPixmap(self.image_paths[self.current_image_index])
+        if pixmap.isNull():
+            print("Error loading image")
+            return
+
+        # Resizing the image
+        pixmap = pixmap.scaledToWidth(200)
+        self.label.setPixmap(pixmap)
+
+        # Removing padding around the image
+
+        self.label.setContentsMargins(0, 0, 0, 0)
 
     def show_quote(self):
         quotes = [
@@ -62,6 +72,11 @@ class MotivationalPuppet(QWidget):
         ]
         quote = random.choice(quotes)
         self.notification_label.setText(quote)
+
+    def change_avatar(self):
+        # Incrementing the index and cycle back to 0 if it exceeds the number of images
+        self.current_image_index = (self.current_image_index + 1) % len(self.image_paths)
+        self.load_image()
 
     def start_quote_timer(self):
         self.quote_timer.start(1000)  # timer for quote
